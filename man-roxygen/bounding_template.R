04c656948d3c1f6474be1760513d567dfe163519
colemonnahan <monnahc@uw.edu>
2015-10-22 11:19:00 -0700

    initial file dump

---
+++ man-roxygen/bounding_template.R
@@ -0,0 +1,13 @@
+#' The bounding functions, with their inverses and derivatives.
+#'
+#' @details If \code{hbf=0} then the function is minb+(maxb-minb)/(1+exp(-x)),
+#' otherwise it is minb+(maxb-minb)*(.5*sin(x*pi/2)+.5).
+#'
+#' @export
+#' @param x The input value.
+#' @param minb The minimum bound.
+#' @param maxb The maximum bound.
+#' @param hbf The hybrid bounded flag. Determines which function is used.
+#' @return Numeric function value.
+#' @author Cole C. Monnahan
+#' @section Note: These functions are named as in the ADMB source code.
